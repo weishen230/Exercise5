a90a275a97f5ec5ddb5038b98c40884ac08c88b7
weishen230 <weishengood@gmail.com>
2019-12-27 11:17:54 +0800

    Initial commit

---
+++ app/src/main/java/com/example/exercise5/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.example.exercise5
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.GestureDetector
+import androidx.core.content.edit
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_main.*
+import my.edu.tarc.exercise5.CountViewModel
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    private var returnMe : String =""
+    private var like : String = ""
+    private var dislike : String = ""
+
+    lateinit var countViewModel: CountViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        countViewModel = ViewModelProviders.of(this).get(CountViewModel::class.java);
+        var like = 0
+        var dislike =0
+        var increase = 0
+
+        imageViewLike.setOnClickListener{
+
+            countViewModel.like(1)
+
+            like = countViewModel.returnLike()
+
+            textViewLike.setText("" + like)
+
+        }
+
+        imageViewDislike.setOnClickListener{
+
+            countViewModel.dislike(1)
+
+            dislike = countViewModel.returnDislike()
+
+            textViewDislike.setText("" + dislike)
+
+        }
+
+        //textViewLike.setText("" + countViewModel.returnLike())
+        //textViewDislike.setText("" + countViewModel.returnDislike())
+
+
+    }
+
+
+
+    override fun onPause() {
+        super.onPause()
+
+        like = countViewModel.returnLike().toString()
+        dislike = countViewModel.returnDislike().toString()
+
+        var sharedPreferences = getSharedPreferences("com.example.exercise5", Context.MODE_PRIVATE)
+        var saveLike = sharedPreferences.getString("like", like)?:return
+        var saveDislike = sharedPreferences.getString("dislike", dislike)?:return
+
+
+        with(sharedPreferences.edit()){
+            putString("like", like)
+            putString("dislike", dislike)
+            apply()
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        var sharedPreferences = getSharedPreferences("com.example.exercise5", Context.MODE_PRIVATE)
+
+        var getLike = sharedPreferences.getString("like", like)
+        var getDislike = sharedPreferences.getString("dislike", dislike)
+
+        if(getLike.equals("") && getDislike.equals(""))
+        {
+            getLike = "0"
+            getDislike = "0"
+        }
+
+        textViewLike.setText(getLike + "")
+        textViewDislike.setText(getDislike + "")
+        countViewModel.setLike(getLike.toString().toInt())
+        countViewModel.setDislike(getDislike.toString().toInt())
+    }
+
+
+
+
+
+
+
+}
